79e6bce38421f4dfaea209c4515a45f57bceb3f5
Avishai Cohen <avishaic2016@gmail.com>
2024-01-17 18:43:47 +0200

    finishing footer

--- src/cmps/AppFooter.vue
+++ src/cmps/AppFooter.vue
@@ -3,7 +3,7 @@
 
         <img class="footer-decoration" src="https://nonsense.jp/_image?href=%2F_astro%2Ffooter.bb9becd1.svg&f=svg" alt="">
         <footer class="app-footer">
-            <div>
+            <div class="site-info">
                 <ul class="clean-list">
                     <li>About us</li>
                     <li>Refund policy</li>
@@ -13,7 +13,7 @@
 
             <div class="newsletter">
                 <h1>Newsletter signup</h1>
-                <p>Stay in the loop about the latest nonsense drops and special eveants.</p>
+                <p>Stay in the loop about the latest nonsense drops and special events.</p>
                 <form>
                     <input type="email" placeholder="Your e-mail">
                     <button>Sign up</button>
@@ -22,11 +22,14 @@
 
             <img src="https://nonsense.jp/_image?href=%2F_astro%2Ficon.fc3a7207.svg&f=svg" alt="">
 
-            <p>Copyright © 2023 nonsense japan</p>
+            <p><span class="tx-cap">Copyright</span> © 2023 nonsense japan</p>
 
-            <ul class="social-links-container clean-list">
-                <p>social-links</p>
-            </ul>
+            <div class="social-links-container clean-list">
+                <a href="https://twitter.com/nonsensejapan"><font-awesome-icon :icon="['fab', 'twitter']" /></a>
+                <a href="https://www.instagram.com/nonsense_jp"><font-awesome-icon :icon="['fab', 'instagram']" /></a>
+                <a href="https://www.tiktok.com/@nonsense_jp"><font-awesome-icon :icon="['fab', 'tiktok']" /></a>
+                <a href="mailto:connect@nonsense.jp"><font-awesome-icon :icon="['fas', 'envelope']" /></a>
+            </div>
         </footer>
     </section>
 </template>
@@ -43,24 +46,39 @@ export default {}
 
 .app-footer {
     display: grid;
-    grid-template-columns: repeat(3, 1fr);
-    row-gap: 20px;
+    // grid-template-columns: repeat(3, 1fr);
+    grid-template-columns: 1fr 2fr 1fr;
+    row-gap: 70px;
 
-    padding-inline: 3rem;
-    padding-block: 3.5rem;
+    padding-inline: 4rem;
+    padding-block-end: 2.25rem;
+    padding-block-start: 1.75rem;
+
     background-color: rgb(252 238 10);
 
     &>* {
         text-transform: uppercase;
-
     }
 
-    li {
-        font-size: 30px;
+    .site-info {
+        align-self: center;
+        line-height: 2.25rem;
+
+        li {
+            font-size: 30px;
+            transition: 0.2s;
+            cursor: pointer;
+
+            &:hover {
+                color: lighten($color: black, $amount: 35);
+            }
+        }
     }
 
     .newsletter {
         text-align: center;
+        align-self: center;
+        line-height: 1.75rem;
 
         h1 {
             font-size: 3.75rem;
@@ -70,16 +88,20 @@ export default {}
 
         p {
             font-size: 20px;
-            padding-block: 8px;
+            // padding-block: 8px;
         }
     }
 
     form {
+        padding-block: 1rem;
 
         input {
             background-color: rgb(252 238 10);
             border: 1px black solid;
-            // font-family: inherit;
+            font-family: inherit;
+            font-size: 1.25rem;
+            line-height: 1.75rem;
+            padding-inline: 0.75rem;
         }
 
         button {
@@ -91,7 +113,7 @@ export default {}
             font-size: 1.3rem;
             text-transform: uppercase;
 
-            padding-inline: 1.3rem;
+            padding-inline: 1.4rem;
             padding-block: 0.3rem;
             margin-inline-start: 10px;
 
@@ -110,9 +132,24 @@ export default {}
 
     .social-links-container {
         grid-column: 3;
-        text-align: center;
-        width: 254px;
+
+        display: flex;
+        width: 16rem;
+        justify-content: center;
         margin-inline-start: auto;
+
+        gap: 1.25rem;
+
+        &>* {
+            font-size: 1.125rem;
+            line-height: 1.75rem;
+            color: black;
+            transition: 0.2s;
+
+            &:hover {
+                color: lighten($color: black, $amount: 35);
+            }
+        }
     }
 }
 </style>
